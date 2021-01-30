5c7f25cfc902e1a9daef2c221d8b5e6275c4d9c1
Sören Kirchner <sk@tologo.de>
2021-01-30 21:40:52 +0100

    Initial Commit

---
+++ SearchBarSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchBarSwiftUI
+//
+//  Created by Sören Kirchner on 30.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchBarSwiftUI/SearchBarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarSwiftUIApp.swift
+//  SearchBarSwiftUI
+//
+//  Created by Sören Kirchner on 30.01.21.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
